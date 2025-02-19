1f4fcf7c8c953a1e82ad8d87c7addd2ce30b7346
Jaffenheimer <jfog20@student.aau.dk>
2025-02-19 11:51:14 +0100

    Added sample test as well as how a testfile should look

---
+++ Autoscaler.Tests/SampleTests.cs
@@ -0,0 +1,16 @@
+namespace Autoscaler.Tests;
+[TestFixture]
+public class SampleTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test()]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+   
+}
