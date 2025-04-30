04d111fecb6c88ef453c3044118eb0b4343ed4f7
Jaffenheimer <74659222+Jaffenheimer@users.noreply.github.com>
2025-04-30 16:12:27 +0200

    Add ckpt column and formatted files

--- Autoscaler.DbUp/Scripts/0003-HistoricTable.sql
+++ Autoscaler.DbUp/Scripts/0003-HistoricTable.sql
@@ -2,9 +2,9 @@ START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS HistoricData
 (
-    Id           UUID NOT NULL PRIMARY KEY,
-    ServiceId    UUID NOT NULL,
-    CreatedAt    timestamp   NOT NULL,
-    HistoricData jsonb        NOT NULL
+    Id           UUID      NOT NULL PRIMARY KEY,
+    ServiceId    UUID      NOT NULL,
+    CreatedAt    timestamp NOT NULL,
+    HistoricData jsonb     NOT NULL
 );
 COMMIT;
